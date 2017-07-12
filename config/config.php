d600a92dedd575c78f3bdb08f601dceac67bacc8
Clint Ellyson <clintellyson@hotmail.com>
2017-07-12 16:29:56 -0700

    updated config with gps table

--- config/config.php
+++ config/config.php
@@ -7,32 +7,22 @@ $config = (object) array(
 'host'  => "192.168.1.12",
 #DATABASE
 'port'  => "3306",
-'user'  => "db_user",
+'user'  => "db_user", //recommend using a read-only db account
 'pass'  => 'db_pass',
 'base'  => "characters",
 'table' => "characters",
-//revision will determine what map to load
+'gps'   => "character_gps",
+#REVISION - will determine what map to load
 'revision' => "3x",
 );
 
-$con = new mysqli($config->host, $config->user, $config->pass, $config->base, $config->port);
-if ($con->connect_error){
-  die("There was a problem connecting to the database:" . $con->connect_error);
+$DB = (object) array(
+'core'=> new mysqli($config->host, $config->user, $config->pass, $config->base, $config->port),
+'gps' => new mysqli($config->host, $config->user, $config->pass, $config->gps, $config->port),
+);
+
+if ($DB->core->connect_error){
+  die("There was a problem connecting to the database:" . $DB->core->connect_error);
 } 
-
-$result = $con->query('SELECT name, gender, class, race, level from '.$config->table.'');
-
-if ($result->num_rows > 0)
-{
-  while($row = $result->fetch_assoc())
-  {
-    echo $row["name"];
-    echo "<br>";
-  }
-}
-else
-{
-  echo "no users online";
-}
 
 ?>
